a50ea84729d7f2085172a7d3f88e2629d82182f6
alex-norris <anor013@gmail.com>
2022-11-08 23:11:11 -0500

    Create probability.py

---
+++ probability.py
@@ -0,0 +1,16 @@
+import random
+
+committee = []
+committeeSize = 4
+
+for j in range(10000):  # how many iterations?
+    profs = [0,0,0,0,1,1,1,1,1,1]  # 4 women + 6 men, reset after each run
+    menCount = 0  # reset after each committee
+    for i in range(committeeSize):
+        # remove random prof and add to running total if man:
+        menCount += profs.pop(random.randint(0,len(profs)-1))
+    # add the number of men in this committee to running tally:
+    committee.append(menCount)
+
+# average number of men per committee, as a ratio
+print(sum(committee)/len(committee)/committeeSize)
